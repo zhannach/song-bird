7376a087c619b7d098f09b380c80c37b890845d3
Zhanna <zhanna01992@gmail.com>
2022-11-16 17:33:05 +0200

    feat: add audio functionality in quize.jsl

---
+++ song-bird/src/quiz.js
@@ -0,0 +1,138 @@
+  import '@/assets/styles/quiz.scss';
+import '@/play'
+
+// create audio player
+const mainPlayer = document.querySelector('.random-bird'); 
+initPlayer(mainPlayer)
+
+export function initPlayer(randomSection) {
+  const audio = randomSection.querySelector('.bird__audio');
+  const duration = randomSection.querySelector('.duration')
+  const sliderAudio = randomSection.querySelector('.timebar__line')
+  const currentTime = randomSection.querySelector('.current-time')
+  const btnPlay = randomSection.querySelector('.audio-player__button')
+  const btnVolume = randomSection.querySelector('.btn-volume')
+  const volumeSlider = randomSection.querySelector('.volume-slider')
+  const progress = randomSection.querySelector('.timebar__progress')
+
+  let playState = 'play';
+  let muteState = 'unmute';
+
+  audioEvent(audio)
+  sliderEvent(sliderAudio)
+  playAudio(btnPlay)
+  muteAudio(btnVolume)
+  sliderEvent(sliderAudio)
+  changeVolume(volumeSlider)
+
+  // uptade audio time
+  setInterval(() => {
+      currentTime.textContent = convertTime(audio.currentTime)
+      progress.style.width = audio.currentTime / audio.duration * 100 + "%";
+  }, 500);
+
+  function audioEvent(elem) {
+    elem.addEventListener('loadedmetadata', () => {
+      showDuration()
+    });
+    elem.addEventListener('timeupdate', () => {
+      sliderAudio.value = Math.floor(audio.currentTime);
+    })  
+  
+    if (audio.readyState > 0) {
+      showDuration();
+      setEndSleder()
+    } else {
+      audio.addEventListener('loadedmetadata', () => {
+      showDuration();
+      setEndSleder()
+      })
+    }
+  }
+  
+  function sliderEvent(elem) {
+    elem.addEventListener('input', () => {
+      currentTime.textContent = convertTime(sliderAudio.value);
+    })
+    
+    elem.addEventListener('change', () => {
+      audio.currentTime = sliderAudio.value;
+    })
+    
+    elem.addEventListener('click', e => {
+      const sliderWidth = window.getComputedStyle(sliderAudio).width
+      const timeToSeek = e.offsetX / parseInt(sliderWidth) * audio.duration
+      audio.currentTime = timeToSeek
+    }, false)
+  }
+  
+  function convertTime(secs) {
+    const min = Math.floor(secs / 60);
+    const sec = Math.floor(secs % 60);
+    const returnedSec = sec < 10 ? `0${sec}` : `${sec}`;
+    return `0${min}:${returnedSec}`;
+  }
+  
+  function showDuration() {
+    duration.textContent = convertTime(audio.duration);
+  }
+  
+  function setEndSleder() {
+     sliderAudio.max = Math.floor(audio.duration);
+  }
+    //mute audio
+  function muteAudio(btn) {
+    btn.addEventListener('click', () => {
+      if(muteState === 'unmute') {
+        audio.muted = true;
+        muteState = 'mute';
+        btnVolume.firstElementChild.classList.toggle('active')
+        btnVolume.lastElementChild.classList.toggle('active')
+      } else {
+        audio.muted = false;
+        muteState = 'unmute';
+        btnVolume.firstElementChild.classList.toggle('active')
+        btnVolume.lastElementChild.classList.toggle('active')
+      }
+    })
+  }
+  
+  // play audio
+  function playAudio(btn) {
+    btn.addEventListener('click', () => {
+      if(playState === 'play') {
+        audio.play();
+        playState = 'pause';
+        btn.firstElementChild.classList.toggle('pause')
+        btn.lastElementChild.classList.toggle('pres')
+      } else {
+        playState = 'play';
+        audio.pause()
+        btn.firstElementChild.classList.toggle('pause')
+        btn.lastElementChild.classList.toggle('pres')
+      }
+    })
+  }
+  
+  //change volume
+  function changeVolume(slider) {
+    slider.addEventListener("change", function(e) {
+      audio.volume = e.currentTarget.value / 100;
+    })
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
